92eca80846b11028a6e1d22c977438337265eabe
TrueDie3 <t.pierzchalski0@gmail.com>
2023-03-23 13:33:12 +0100

    update
    Add operation functions to calculator.
    Remain a lot of work! :(

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,69 @@
 const screen = document.querySelector(".screen");
-const para = document.createElement("p");
-para.textContent = "pozdro";
-function writeOnScreen() {
-	screen.appendChild(para);
+
+// Buttons
+const buttonsNum = document.querySelectorAll("button");
+buttonsNum.forEach(button => {
+	button.addEventListener("click", () => {
+		screen.value += button.textContent;
+	});
+});
+const buttonsFunctional = document.querySelectorAll(".functional");
+buttonsFunctional.forEach(button => {
+	button.addEventListener("click", a => {
+		screen.value;
+		a = screen.value.slice(0, screen.value.length - 1);
+		screen.removeAttribute("placeholder");
+		screen.value = "";
+		console.log(a);
+	});
+	button.addEventListener("click", b => {
+		screen.value;
+		b = screen.value.slice(0, screen.value.length - 1);
+		console.log(b);
+	});
+});
+const equalButton = document.querySelector(".equal-button");
+
+// Calculator functions \\
+function addition() {
+	screen.value = a + b;
 }
 
-writeOnScreen();
+function subtraction(a, b) {
+	return (screen.value = a - b);
+}
+
+function multiplication(a, b) {
+	return (screen.value = a * b);
+}
+
+function division(a, b) {
+	return (screen.value = a / b);
+}
+
+function percentage(a, b) {
+	return (screen.value = (a / b) * 100 + "%");
+}
+
+function squareRoot(a) {
+	return (screen.value = Math.sqrt(a));
+}
+
+function exponentiation(a, b) {
+	return (screen.value = a ** b);
+}
+
+// Calculator
+function operate() {}
+
+equalButton.addEventListener("click", () => {
+	operate();
+});
+
+// function isEqual() {}
+
+// Reset calc
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", () => {
+	screen.value = "";
+});
