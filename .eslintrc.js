5b2291ef90947657caf8bc71b5be4e3fb4964c2b
TrueDie3 <t.pierzchalski0@gmail.com>
2023-04-04 14:53:51 +0200

    update
    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-use-before-define": [
+      "error",
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        allowNamedExports: false,
+      },
+    ],
+    "prettier/prettier": ["error", { parser: "css" }],
+  },
+};
